371c6483f65dbb4a8e280e931f718884149f381d
Leonardo Duarte <hintl3rgames2015@gmail.com>
2023-09-02 07:33:40 -0300

    Header

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+import React from 'react';
+import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Header from './Components/Header';
+import Footer from './Components/Footer';
+import Home from './Components/Home';
+import Login from './Components/Login/Login';
+
 function App() {
-  return <>App React</>;
+  return (
+    <div>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
