563727c77433acaa571bd7ca4ab506d2c5d62d88
Leonardo Duarte <hintl3rgames2015@gmail.com>
2023-09-02 07:51:17 -0300

    Login Routes

---
+++ src/Components/Forms/Button.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import styles from './Button.module.css';
+const Button = () => {
+  return (
+    <div>
+      <button className={styles.button}></button>
+    </div>
+  );
+};
+
+export default Button;
---
+++ src/Components/Forms/Input.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import styles from './Input.module.css';
+const Button = () => {
+  return (
+    <div>
+      <input className={styles.input} />
+    </div>
+  );
+};
+
+export default Button;
