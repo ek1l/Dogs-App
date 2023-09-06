5944155653f5c201242631c4d0941adff9be5713
Leonardo Duarte <hintl3rgames2015@gmail.com>
2023-09-06 18:17:13 -0300

    Feed Modal 2

--- src/Components/Feed/FeedModal.jsx
+++ src/Components/Feed/FeedModal.jsx
@@ -5,17 +5,20 @@ import { PHOTO_GET } from '../../Api';
 import Error from '../Helper/Erro';
 import Loading from '../Helper/Loading';
 import PhotoContent from '../Photo/PhotoContent';
-const FeedModal = ({ photo }) => {
+const FeedModal = ({ photo, setModalPhoto }) => {
   const { data, error, loading, request } = useFetch();
   React.useEffect(() => {
     const { url, options } = PHOTO_GET(photo.id);
     request(url, options);
   }, [photo, request]);
+  function handleOutsideClick(event) {
+    if (event.target === event.currentTarget) setModalPhoto(null);
+  }
   return (
-    <div className={styles.modal}>
+    <div className={styles.modal} onClick={handleOutsideClick}>
       {error && <Error error={error} />}
       {Loading && <Loading />}
-      {data && <PhotoContent data={data}/>}
+      {data && <PhotoContent data={data} />}
     </div>
   );
 };
