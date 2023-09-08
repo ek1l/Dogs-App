f80157aa4d6d1c0be608f26bb3b1a6ff10bad954
Leonardo Duarte <hintl3rgames2015@gmail.com>
2023-09-08 12:18:20 -0300

    Photo Delete

--- src/Components/Feed/FeedPhotos.jsx
+++ src/Components/Feed/FeedPhotos.jsx
@@ -11,7 +11,8 @@ const FeedPhotos = ({ setModalPhoto }) => {
   React.useEffect(() => {
     async function fetchPhotos() {
       const { url, options } = PHOTOS_GET({ page: 1, total: 6, user: 0 });
-      const { response, json } = await request(url, options);
+      const { json } = await request(url, options);
+      console.log(json);
     }
     fetchPhotos();
   }, [request]);
