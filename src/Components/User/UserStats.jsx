7b03a74570f8bb17caf70ee34c89ffa0c2cd7c6c
Leonardo Duarte <hintl3rgames2015@gmail.com>
2023-09-12 14:48:35 -0300

    Biblioteca que exibe gráfico de acesso

--- src/Components/User/UserStats.jsx
+++ src/Components/User/UserStats.jsx
@@ -1,13 +1,32 @@
 import React from 'react';
 import Head from '../Helper/Head';
+import useFetch from '../../hooks/useFetch';
+import { STATS_GET } from '../../Api';
+import Loading from '../Helper/Loading';
+import Error from '../Helper/Erro';
+import UserStatsGraphs from './UserStatsGraphs';
 
 const UserStats = () => {
-  return (
-    <div>
-      <Head title="Estatísticas" />
-      Estatísticas
-    </div>
-  );
+  const { data, error, loading, request } = useFetch();
+
+  React.useEffect(() => {
+    async function getData() {
+      const { url, options } = STATS_GET();
+      await request(url, options);
+    }
+    getData();
+  }, [request]);
+
+  if (loading) return <Loading />;
+  if (error) return <Error error={error} />;
+  if (data)
+    return (
+      <div>
+        <Head title="Estatísticas" />
+        <UserStatsGraphs data={data} />
+      </div>
+    );
+  else return null;
 };
 
 export default UserStats;
