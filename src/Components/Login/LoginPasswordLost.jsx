59fc3aaddbad6f86c836e375bc364233b8e997e2
Leonardo Duarte <hintl3rgames2015@gmail.com>
2023-09-11 21:27:07 -0300

    Perdeu Senha 1

--- src/Components/Login/LoginPasswordLost.jsx
+++ src/Components/Login/LoginPasswordLost.jsx
@@ -1,11 +1,43 @@
-import React from 'react'
+import React from 'react';
+import Input from '../Forms/Input';
+import Button from '../Forms/Button';
+import useForm from '../../hooks/useForm';
+import useFetch from '../../hooks/useFetch';
+import { PASSWORD_LOST } from '../../Api';
+import Error from '../Helper/Erro';
 
 const LoginPasswordLost = () => {
+  const login = useForm();
+  const { data, loading, error, request } = useFetch();
+  async function handleSubmit(event) {
+    event.preventDefault();
+    if (login.validate()) {
+      const { url, options } = PASSWORD_LOST({
+        login: login.value,
+        url: window.location.href.replace('perdeu', 'resetar'),
+      });
+      const { json } = await request(url, options);
+      console.log(json);
+    }
+  }
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <section>
+      <h1 className="title">Perdeu a senha?</h1>
+      {data ? (
+        <p style={{ color: '#4c1' }}>{data}</p>
+      ) : (
+        <form onSubmit={handleSubmit}>
+          <Input label="Email / Usuário" type="Text" name="login" {...login} />
+          {loading ? (
+            <Button disabled>Enviando...</Button>
+          ) : (
+            <Button> Enviar email</Button>
+          )}
+        </form>
+      )}
+      <Error error={error} />
+    </section>
+  );
+};
 
-export default LoginPasswordLost
+export default LoginPasswordLost;
