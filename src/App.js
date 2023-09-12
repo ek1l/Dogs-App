e987f989b6c6052197aca992d9ede231e9523e72
Leonardo Duarte <hintl3rgames2015@gmail.com>
2023-09-11 21:04:10 -0300

    Error 404

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import { UserStorage } from './UserContext';
 import User from './Components/User/User';
 import ProtectedRoute from './Components/Helper/ProtectedRoute';
 import Photo from './Components/Photo/Photo';
+import NotFound from './Components/NotFound';
 function App() {
   return (
     <div>
@@ -27,6 +28,7 @@ function App() {
               }
             />
             <Route path="foto/:id" element={<Photo />} />
+            <Route path="*" element={<NotFound />} />
           </Routes>
           <Footer />
         </UserStorage>
--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -5,7 +5,8 @@ import LoginCreate from './LoginCreate';
 import LoginPasswordLost from './LoginPasswordLost';
 import LoginPasswordReset from './LoginPasswordReset';
 import { UserContext } from '../../UserContext';
-import styles from './Login.module.css'
+import styles from './Login.module.css';
+import NotFound from '../NotFound';
 
 const Login = () => {
   const { login } = React.useContext(UserContext);
@@ -13,12 +14,13 @@ const Login = () => {
   return (
     <section className={styles.login}>
       <div className={styles.forms}>
-      <Routes>
-        <Route path="/" element={<LoginForm />} />
-        <Route path="criar" element={<LoginCreate />} />
-        <Route path="perdeu" element={<LoginPasswordLost />} />
-        <Route path="resetar" element={<LoginPasswordReset />} />
-      </Routes>
+        <Routes>
+          <Route path="/" element={<LoginForm />} />
+          <Route path="criar" element={<LoginCreate />} />
+          <Route path="perdeu" element={<LoginPasswordLost />} />
+          <Route path="resetar" element={<LoginPasswordReset />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
       </div>
     </section>
   );
